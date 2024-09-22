4737f27821aa12ca8fec6df2bfc31bec0de19e49
Mazoni <lipemazonisilva@gmail.com>
2024-09-22 15:06:42 -0300

    update

--- src/main/java/com/shoope/api/ApiApplication.java
+++ src/main/java/com/shoope/api/ApiApplication.java
@@ -2,9 +2,8 @@ package com.shoope.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
