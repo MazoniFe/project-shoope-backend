967466c9d23d8be7d7bf0d4524ab954e1f3209ee
Mazoni <lipemazonisilva@gmail.com>
2024-09-22 16:14:55 -0300

    update

--- src/main/java/com/shoope/api/service/CorsConfiguration.java
+++ src/main/java/com/shoope/api/service/CorsConfiguration.java
@@ -2,17 +2,18 @@ package com.shoope.api.service;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
+
 }
